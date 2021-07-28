54e531f804eb314fc5a9eb526467317a72757273
Jackgoers96 <81663225+Jackgoers96@users.noreply.github.com>
2021-07-28 06:55:18 -0500

    whatever

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,14 @@ var header = $('header');
 function buildWeatherResults(data) {
 
     console.log(data)
+    //next line empties the code for new search
     $('.weatherCard').empty();
-    //var farenheit replaces line 12 (data.main.temp)
 
 for(i = 0; i < data.list.length; i++) {
     var forecast = data.list[i];
+        //var farenheit replaces line 12 (data.main.temp), uses Farenheit instead of Kelvin
     var farenheit = Math.round(((forecast.main.temp-273.15)*1.8)+32);
+    //Sets the interval to once a day, by going every 24 hours, based on the 3 hour interval given by API
 if (i % 8 == 3 ){ 
     
     console.log(forecast.dt_txt, farenheit, forecast.weather[0].description);
@@ -17,14 +19,20 @@ if (i % 8 == 3 ){
     header.text(data.city.name);
     header.addClass('fancy');
     var temp = $('<div>');
-    temp.text(farenheit);
-    temp.addClass('fancySmol')
     $('.weatherCard').append(header);
-    $('.fancy').append(temp);
 
+
+        }
+    }
+    if (forecast.weather[0].description == "broken clouds") {
+        $("<img src='./assets/images/brokenClouds.png'>").appendTo('.fancy');
 }
+
+temp.text(farenheit);
+temp.addClass('fancySmol')
+$('.fancy').append(temp);
 }
-}
+
 //Look for a kalvin converter
 
 $('#search-form').on('submit', function(e){
